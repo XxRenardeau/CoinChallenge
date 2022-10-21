99ea76460ed5c57489e0e732557e81872de89abe
XxRenardeau <108811486+XxRenardeau@users.noreply.github.com>
2022-10-21 14:13:27 +0200

    Submit d'avant cours

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -6,56 +6,101 @@ public class CharacterController : MonoBehaviour
 {
     public Rigidbody rigidbody;
     public Animator animator;
+    public float JumpForce = 1;
 
     public float AccelerationSpeed = 1;
     void Start()
     {
+        //Cursor.lockState = CursorLockMode.Confined;
         
+
     }
 
-    
+
     void Update()
     {
         CheckInput();
+        Cursor.visible = false;
+    }
+    void CheckInput() //sers a gerer les input utilisateurs via des commandes
+    {
+        if (Input.GetKey(KeyCode.Z)) { MoveForward(); }
+        if (Input.GetMouseButtonDown(0)) { StartCoroutine(MousePanCorout()); }
+        if (Input.GetKey(KeyCode.D)) { MoveRight(); }
+        if (Input.GetKey(KeyCode.S)) { MoveBack(); }
+        if (Input.GetKey(KeyCode.Q)) { MoveLeft(); }
+        if (Input.GetKeyDown(KeyCode.Space)) { MoveJump(); }
+        if (Input.GetMouseButtonDown(1)) { Tapper(); }
+        if (Input.GetMouseButtonUp(1)) { animator.SetBool("IsTapping", false); }
+        if (Input.anyKey == false) { animator.SetBool("IsWalking", false); /*animator.SetBool("IsTapping", false);*/ animator.SetBool("IsJumping", false); }
+
+
+
 
     }
-    void CheckInput(){
-        if(Input.GetKey(KeyCode.Z)){
-
-            //Debug.Log("Avancer");
-            MoveForward();
-        }
-        else{animator.SetBool("IsWalking",false);}
-        if(Input.GetMouseButtonDown(0)){
-            StartCoroutine(MousePanCorout());
-           
-        }
-        
-    
-    }
-    void MoveForward(){
-        animator.SetBool("IsWalking",true);
+    void MoveForward()
+    {
+        animator.SetBool("IsWalking", true);
         Vector3 Velocity = rigidbody.velocity;
         Velocity += transform.forward * AccelerationSpeed * Time.deltaTime;
         rigidbody.velocity = Velocity;
 
 
     }
-    void Turn(float Direction){
-        transform.Rotate(new Vector3(0,Direction * Time.deltaTime,0));
+    void MoveRight()
+    {
+        animator.SetBool("IsWalking", true);
+        Vector3 Velocity = rigidbody.velocity;
+        Velocity += transform.right * AccelerationSpeed * Time.deltaTime;
+        rigidbody.velocity = Velocity;
+    }
+    void MoveLeft()
+    {
+        animator.SetBool("IsWalking", true);
+        Vector3 Velocity = rigidbody.velocity;
+        Velocity -= transform.right * AccelerationSpeed * Time.deltaTime;
+        rigidbody.velocity = Velocity;
+    }
+    void MoveBack()
+    {
+        animator.SetBool("IsWalking", true);
+        Vector3 Velocity = rigidbody.velocity;
+        Velocity -= transform.forward * AccelerationSpeed * Time.deltaTime;
+        rigidbody.velocity = Velocity;
+    }
+    void MoveJump()
+    {
+        /*animator.SetBool("IsWalking", true);
+        Vector3 Velocity = rigidbody.velocity;
+        Velocity += transform.up * JumpForce * Time.deltaTime;
+        rigidbody.velocity = Velocity;*/
+        rigidbody.AddForce(Vector3.up * JumpForce);
+        animator.SetBool("IsJumping", true);
 
     }
-    IEnumerator MousePanCorout(){
-            float MousePosX = Input.mousePosition.x;
+    void Tapper()
+    {
+        animator.SetBool("IsTapping", true);
+    }
 
-        while(Input.GetMouseButton(0)){
+    void Turn(float Direction)
+    {
+        transform.Rotate(new Vector3(0, Direction * Time.deltaTime, 0));
+
+    }
+    IEnumerator MousePanCorout()
+    {
+        float MousePosX = Input.mousePosition.x;
+
+        while (Input.GetMouseButton(0))
+        {
             float Delta = Input.mousePosition.x - MousePosX;
             MousePosX = Input.mousePosition.x;
-             
+
             Debug.Log(Delta);
 
 
-            Turn(Delta * 5000 *Time.deltaTime);
+            Turn(Delta * 5000 * Time.deltaTime);
 
             yield return null;
 
